8e43bb5ee35e9dfc26b5e50cfd682ab381568746
BrancuAlexandru <alexandrubrancu@yahoo.com>
2023-05-09 04:23:53 +0300

    Added Firebase.

---
+++ src/app/app.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Document</title>
+      </head>
+      <body>
+        <div></div>
+      </body>
+    </html>
+  )
+}
---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import App from './app.tsx';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <App />
+  )
+}
